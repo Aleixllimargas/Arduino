58d75b40041c7997e010b5bf26ee63d27e827b9f
Aleixllimargas <aleix.llimargas@gmail.com>
2021-11-29 16:01:25 +0100

    Update Ex10_Math_Is_Fun.ino

--- 002 Tipus de Dades i Operadors/Ex10_Math_Is_Fun/Ex10_Math_Is_Fun.ino
+++ 002 Tipus de Dades i Operadors/Ex10_Math_Is_Fun/Ex10_Math_Is_Fun.ino
@@ -8,20 +8,27 @@
 //********* Includes *************************************************************
 
 //********* Variables ************************************************************
-int test = 32767;
+int drive_gb = 100;
+long drive_mb;
+
 
 //********* Setup ****************************************************************
-void setup()              // run once, when the sketch starts
+void setup()               //run once, when the sketch starts
 {
-  Serial.begin(9600);     //set up Serial library at 9600 bps
-  Serial.print("Test value is: ");
-  Serial.println(test);
+  Serial.begin(9600);       //set up Serial library at 9600 bps
 
-  test = test + 1;
- 
-  Serial.print("Now it is: ");
-  Serial.println(test);
+  Serial.print("Your HD is ");
+  Serial.print(drive_gb);
+  Serial.println(" GB large.");
+
+  drive_mb = drive_gb;
+  drive_mb = drive_mb * 1024;
+
+  Serial.print("It can store ");
+  Serial.print(drive_mb);
+  Serial.println(" Megabytes!");
 }
+
 
 //********* Loop *****************************************************************
 void loop()
