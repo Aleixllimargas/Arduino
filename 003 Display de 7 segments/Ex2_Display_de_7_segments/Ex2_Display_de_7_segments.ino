e4b5b077533777822fe7fa7a849bc2b043467c0a
Aleixllimargas <aleix.llimargas@gmail.com>
2021-12-20 16:45:19 +0100

    Create Ex2_Display_de_7_segments.ino

---
+++ 003 Display de 7 segments/Ex2_Display_de_7_segments/Ex2_Display_de_7_segments.ino
@@ -0,0 +1,144 @@
+/*********************************************************************************
+**                                                                              **
+**                             Display de 7 segments                            **
+**                                                                              **
+** Nom: Aleix Llimargas                                       Data: 20/12/2021  **
+*********************************************************************************/
+
+//********* Includes *************************************************************
+
+//********* Variables ************************************************************
+const byte segA = 2;          
+const byte segB = 3;          
+const byte segC = 4;          
+const byte segD = 5;          
+const byte segE = 6;          
+const byte segF = 7;         
+const byte segG = 8;
+const int pausa = 500;
+//********* Setup ****************************************************************
+void setup()
+{
+  pinMode(segA, OUTPUT);
+  pinMode(segB, OUTPUT);
+  pinMode(segC, OUTPUT);
+  pinMode(segD, OUTPUT);
+  pinMode(segE, OUTPUT);
+  pinMode(segF, OUTPUT);
+  pinMode(segG, OUTPUT);
+
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, LOW);
+  digitalWrite(segC, LOW);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+}
+//********* Loop *****************************************************************
+void  loop ()      // necessitem que estigui aquí encara que estigui buit
+{
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, LOW);
+
+  delay(pausa);
+
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+
+  delay(pausa);
+
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, LOW);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, HIGH);
+
+  delay(pausa);
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, HIGH);
+
+  delay(pausa);
+
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+
+  delay(pausa);
+
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, LOW);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+
+  delay(pausa);
+
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, LOW);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+
+  delay(pausa);
+
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+
+  delay(pausa);
+  
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+
+  delay(pausa);
+
+  
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+
+  delay(pausa);
+
+ 
+}
+
+//********** Funcions *************************************************************
